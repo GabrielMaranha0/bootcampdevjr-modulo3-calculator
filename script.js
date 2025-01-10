2b722cac2b1fe05c0f567998a71fab89667a0b5f
GabrelMaranha0 <gabriel.rod.maranhao@gmail.com>
2025-01-10 14:41:08 -0300

    closes #3 - add function sum()

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function sum (){
+function sum (){//função soma 
     var num1 = parseFloat(document.getElementById("num1").value);//recebe os ID's dos inputs
     var num2 = parseFloat(document.getElementById("num2").value);//altera de string para float
 
