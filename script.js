b42b51ef6df6c52db0e42440547e3a2ebe36b059
GabrelMaranha0 <gabriel.rod.maranhao@gmail.com>
2025-01-10 14:40:14 -0300

    closes #2 - add css

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function sum (){
-    var num1 = document.getElementById("num1").value;//recebe os ID's dos inputs
-    var num2 = document.getElementById("num2").value;
+    var num1 = parseFloat(document.getElementById("num1").value);//recebe os ID's dos inputs
+    var num2 = parseFloat(document.getElementById("num2").value);//altera de string para float
 
     var output = num1 + num2;
 
     document.getElementById("output").value = output; 
 }
+
