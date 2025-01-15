f8f698c1aba14b9df947b55f92f5b580a050096d
GabrelMaranha0 <gabriel.rod.maranhao@gmail.com>
2025-01-14 21:39:39 -0300

    Added new mathematical operations

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ function sum (){//função soma
     document.getElementById("output").value = output; 
 }
 
+function sub (){//função soma 
+    var num1 = parseFloat(document.getElementById("num1").value);//recebe os ID's dos inputs
+    var num2 = parseFloat(document.getElementById("num2").value);//altera de string para float
+
+    var output = num1 - num2;
+
+    document.getElementById("output").value = output; 
+
+}function division (){//função soma 
+    var num1 = parseFloat(document.getElementById("num1").value);//recebe os ID's dos inputs
+    var num2 = parseFloat(document.getElementById("num2").value);//altera de string para float
+
+    var output = num1 / num2;
+
+    document.getElementById("output").value = output; 
+
+}function multiply (){//função soma 
+    var num1 = parseFloat(document.getElementById("num1").value);//recebe os ID's dos inputs
+    var num2 = parseFloat(document.getElementById("num2").value);//altera de string para float
+
+    var output = num1 * num2;
+
+    document.getElementById("output").value = output; 
+}
