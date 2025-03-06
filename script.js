5e49556947bc14ac5c157bf5f631b07a8ab00d36
GabrelMaranha0 <gabriel.rod.maranhao@gmail.com>
2025-03-06 07:11:23 -0300

    closes #6 - Added history

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function calc (op){
     }
     document.getElementById("output").value = output;
 
-    var newHistory = "<div>"+num1+" "+op+" "+num2+" = "+output+ "</div>";
+    var newHistory = "<p>"+num1+" "+op+" "+num2+" = "+output+ "</p>";
     var history = document.getElementById("history");
 
     history.innerHTML = newHistory + document.getElementById("history").innerHTML //aqui ele vai mostrar o histórico de operações realizadas.
